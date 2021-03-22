b4c2f9936997cb6e05ffa295e19d6654854dd0a9
suhyang-lee <suhyang.dev@gmail.com>
2021-03-22 21:06:41 +0900

    [add] fonts asset 추가, reset.css 추가, theme.js 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,27 @@
+const theme = {
+  colors: {
+    black100: '#1E1E1E',
+    gray100: '#EDEEF0',
+    gray200: '#B0B0B0',
+    blue100: '#50A0E0',
+    blue200: '#A4D4FA',
+  },
+  fontWeight: {
+    black: 700,
+    bold: 400,
+    light: 300,
+    thin: 100,
+  },
+  padding: {
+    leftAndRight30: '0 3rem',
+  },
+  margin: {
+    margin10: 1,
+    margin20: 2,
+    margin30: 3,
+    margin40: 4,
+    margin50: 5,
+  },
+};
+
+export default theme;
