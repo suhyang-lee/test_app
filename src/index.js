76bf60bb61de8cafd1765290ce7ccc71a48aef5f
suhyang-lee <suhyang.dev@gmail.com>
2021-03-22 21:07:44 +0900

    [update] 테마 provider 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/reset.css';
+import { ThemeProvider } from '@emotion/react';
 import App from './pages/App';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
